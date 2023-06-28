e82445eec8e5934dabf11c46dbfc7221561238c6
eya46 <644310932@qq.com>
2023-06-28 10:57:00 +0800

    555...

--- plugins/show.py
+++ plugins/show.py
@@ -1,6 +1,6 @@
 from nonebot import on_message, logger
 
-from nonebot.adapters.club255 import PostEvent, Bot, FaceEnum, NextLine, FaceMsg  # type: ignore
+from nonebot.adapters.club255 import NewPostEvent, Bot, FaceEnum, NextLine, FaceMsg  # type: ignore
 from nonebot.adapters.club255.event import NewNicePostEvent  # type: ignore
 from nonebot.adapters.club255.permission import POST_NEW_USER  # type: ignore
 
@@ -8,5 +8,5 @@ show = on_message(block=False)
 
 
 @show.handle()
-async def show_handle(bot: Bot, event: PostEvent):
+async def show_handle(bot: Bot, event: NewPostEvent):
     logger.info(f"{event.post.title}[{event.post.postId}]")
